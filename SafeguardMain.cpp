b7f6650c4a770c0729e4d3d09e6170cd58fde279
JOE <jphinspace@gmail.com>
2015-03-20 15:57:22 -0400

    Added frame counter code, minor structure improvements

--- SafeguardMain.cpp
+++ SafeguardMain.cpp
@@ -2,18 +2,6 @@
 #include <SDL.h>
 #include <stdio.h>
 
-
-//Key press surfaces constants
-enum KeyPressSurfaces
-{
-    KEY_PRESS_SURFACE_DEFAULT,
-    KEY_PRESS_SURFACE_UP,
-    KEY_PRESS_SURFACE_DOWN,
-    KEY_PRESS_SURFACE_LEFT,
-    KEY_PRESS_SURFACE_RIGHT,
-    KEY_PRESS_SURFACE_TOTAL
-};
-
 //starts SDL, creates window
 bool init();
 
@@ -21,8 +9,9 @@ bool init();
 bool loadMedia();
 
 //frees media, shut down SDL
-void close();
-
+void close();//FPS thread and its spinlock (for fps counter variable)int fpsworker(void* data);
+SDL_SpinLock gDataLock = 0;
+//Event handlerSDL_Event e;
 //window we're rendering to
 SDL_Window* gWindow = NULL;
 
@@ -34,12 +23,10 @@ SDL_Surface* gYWing = NULL;
 
 //screen dimension constants
 const int SCREEN_WIDTH = 640;
-const int SCREEN_HEIGHT = 480;
+const int SCREEN_HEIGHT = 480;//main loop end flagbool quit;//for fps counterint framesThisSecond; //this is displayed by main functionint countedFrames; //constantly increments, set to 0 by fpsworker once per second
 
 //Keeps track of time between steps
-LTimer stepTimer;
-
-bool init()
+LTimer stepTimer;bool init()
 {
     //Initialization flag
     bool success = true;
@@ -76,17 +63,35 @@ bool loadMedia()
     //load image
     gYWing = SDL_LoadBMP()
 }
-
-void processInput()
+void close()
 {
-    //Check for various keycodes/mouse input
+	//Free loaded images
+	gSplashTexture.free();
 
-    //2 options:
-        //Events: make event handler for each key corresponding with player action
-        //Polling: every frame check keyboard which keys are down
+	//Destroy window
+	SDL_DestroyRenderer( gRenderer );
+	SDL_DestroyWindow( gWindow );
+	gWindow = NULL;
+	gRenderer = NULL;
 
-    //Best advice: abstract away the polling/event stuff so can change it if we want to
-    //Then to start with, just pick easiest option (maybe polling, look into SDL events though in tutorials)
+	//Quit SDL subsystems
+	IMG_Quit();
+	SDL_Quit();
+}int fpsworker( void* data )
+{
+    //Work continuously	SDL_AtomicLock( &gDataLock );	bool fpsquit = quit;	SDL_AtomicUnlock( &gDataLock );	while(!fpsquit)
+	{
+
+		SDL_AtomicLock( &gDataLock );		framesThisSecond = countedFrames;		countedFrames = 0;		SDL_AtomicUnlock( &gDataLock );
+
+		//Wait one second
+		SDL_Delay(1000);
+	}
+
+	return 0;
+}
+void processInput(SDL_Event e)
+{    while(SDL_PollEvent(&e) != 0)    {        if(e.type == SDL_QUIT)        {            SDL_AtomicLock( &gDataLock );            quit = true;            SDL_AtomicUnlock( &gDataLock );        }    }    const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL);    if(currentKeyStates[SDL_SCANCODE_UP])    {        //do something    }
 }
 
 void update()
@@ -106,20 +111,19 @@ void update()
         //object.move (x += xv, y += yv)
 
     //handle collisions
-
     //Move for time step
-    stepTimer.start();
+    stepTimer.start();    //Calculate FPS    timeText.str("");    SDL_AtomicLock( &gDataLock );    timeText << "FPS: " << framesThisSecond;    SDL_AtomicUnlock( &gDataLock );    if(!gFPSTextTexture.loadFromRenderedText(timeText.str().c_str(), fpsColor))    {        printf("Unable to render FPS texture.\n");    }
 }
 
 void render()
-{
+{    //Clear screen    SDL_SetRenderDrawColor(gRenderer, 0xFF, 0xFF, 0xFF, 0xFF);    SDL_RenderClear(gRenderer);
     //draw background
     //draw background objects
     //draw bullets
     //draw npcs
     //draw player
-    //draw asteroids/explosions/effects
-    //flip
+    //draw asteroids/explosions/effects    //draw FPS number (framesThisSecond)    gFPSTextTexture.render((SCREEN_WIDTH - gFPSTextTexture.getWidth())/2, (SCREEN_HEIGHT - gFPSTextTexture.getHeight())/2);
+    //flip    SDL_RenderPresent(gRenderer);    //Increment frame counter    SDL_AtomicLock( &gDataLock );    countedFrames++;    SDL_AtomicUnlock( &gDataLock );
 }
 
 void delay()
@@ -162,10 +166,10 @@ int main( int argc, char* args[] )
             //Wait 2 seconds
             SDL_Delay(2000);
         }
-    }
+    }    quit = false;    countedFrames = 0;    framesThisSecond = 0;    //Start fps counting thread    SDL_Thread* fpsThread = SDL_CreateThread(fpsworker, "FPSThread", NULL);    //FPS text    std::stringstream timeText;    SDL_Color fpsColor = {0,0,0,255};
 
-    #MAIN GAME LOOP
-    while(true)
+    #MAIN GAME LOOP    SDL_AtomicLock( &gDataLock );    bool mainquit = quit;    SDL_AtomicUnlock( &gDataLock );
+    while(!mainquit)
     {
         //Get/process input from user
         processInput();
@@ -176,10 +180,9 @@ int main( int argc, char* args[] )
         //Draw world + objects
         render();
 
-        //Delay (goal: 60 fps)
-        delay();
-    }
-
+        //Delay (goal: 120 fps with vsync)
+        delay();    }
+    SDL_WaitThread(fpsThread, NULL);
     //Destroy window
     SDL_DestroyWindow( window );
 
--- SpaceObject.cpp
+++ SpaceObject.cpp
@@ -1,31 +1,30 @@
-#define _USE_MATH_DEFINES
+#define _USE_MATH_DEFINES#include <SDL.h>
 #include <math.h>
-
-class SpaceObject
-{
-private:
-    float x, y, xv, yv, spd, accel, max_spd;
-    float move_dir, move_dir_spd, move_dir_accel, move_dir_max_spd;
-    float face_dir, face_dir_spd, face_dir_accel, face_dir_max_spd;
-    float timestep; //this might actually need to be an int
-    SDL_Rect bound;
-    void updateVels();
-public:
-    void steerRight();
-    void steerLeft();
-    void moveForward();
-    void getX() { return x; }
-    void getY() { return y; }
+//Player ships, NPCs, asteroids, bullets, explosions, particles
+class SpaceObject{
+    private:
+        float x, y, xv, yv, spd, accel, max_spd;
+        float move_dir, move_dir_spd, move_dir_accel, move_dir_max_spd;
+        float face_dir, face_dir_spd, face_dir_accel, face_dir_max_spd;
+        float timestep;
+        SDL_Rect bound;
+        void updateVelocity();
+    public:
+        void steerRight();
+        void steerLeft();
+        void moveForward();
+        void getX() { return x; }
+        void getY() { return y; }
 };
-
-void SpaceObject::updateVels() {
+//This runs continuously
+void SpaceObject::updateVelocity(){
     if (spd < max_spd)
         spd += accel*timestep;
     xv = spd + cos(move_dir*M_PI/180);
     yv = spd + sin(move_dir*M_PI/180);
 }
-
-void SpaceObject::steerRight() {
+//Generic implementation, set accels to 1 for instant turn
+void SpaceObject::steerRight(){
     if (move_dir_spd < move_dir_max_spd)
     {
         if (move_dir_spd < 0)
@@ -34,7 +33,7 @@ void SpaceObject::steerRight() {
     }
     move_dir += move_dir_spd*timestep;
 }
-void SpaceObject::steerLeft() {
+void SpaceObject::steerLeft(){
     if (move_dir_spd > -move_dir_max_spd)
     {
         if (move_dir_spd > 0)
@@ -42,8 +41,8 @@ void SpaceObject::steerLeft() {
         move_dir_spd -= move_dir_accel*timestep;
     }
     move_dir += move_dir_spd*timestep;
-}
-void SpaceObject::moveForward() {
+}//This pretty much runs continuously too
+void SpaceObject::moveForward(){
     updateVels();
     x += xv*timestep;
     y += yv*timestep;
